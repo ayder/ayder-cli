b466629fe06783ef3af683f4556175990379cf5d
sinan alyuruk <sinan@dbsmedya.com>
2026-03-19 13:28:05 +0300

    refactor: export ChatLoop/ChatCallbacks/ChatLoopConfig from loops package

--- src/ayder_cli/loops/__init__.py
+++ src/ayder_cli/loops/__init__.py
@@ -1,6 +1,13 @@
-"""Shared agent loop base classes."""
+"""Shared agent loop base classes and chat loop."""
 
 from ayder_cli.loops.base import AgentLoopBase
 from ayder_cli.loops.config import LoopConfig
+from ayder_cli.loops.chat_loop import ChatLoop, ChatCallbacks, ChatLoopConfig
 
-__all__ = ["AgentLoopBase", "LoopConfig"]
+__all__ = [
+    "AgentLoopBase",
+    "LoopConfig",
+    "ChatLoop",
+    "ChatCallbacks",
+    "ChatLoopConfig",
+]
